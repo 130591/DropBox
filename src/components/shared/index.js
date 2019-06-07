ca5c3e214dcfd7681509bff1c7bf61e293888cb6
everton paixao <everton.paixao16@gmail.com>
2019-06-07 14:36:45 -0300

    adjustment on default style of components header and sidebar

--- src/components/shared/index.js
+++ src/components/shared/index.js
@@ -1,12 +1,13 @@
-import styled from 'styled-components';
- 
+import styled from "styled-components";
+
 export const Container = styled.div`
   margin: 0 auto;
   width: 100%;
 `;
 
 export const Wrapper = styled.div`
+  width: 100%;
+  min-height: 100%;
   display: flex;
-  width: 100%; 
-  min-height: 100%; 
+  background-color: #45ff;
 `;
--- src/components/sidebar/style.js
+++ src/components/sidebar/style.js
@@ -1,22 +1,27 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const BarContainer = styled.aside`
-  width: 20%;
+  width: 240px;
   height: 100%;
-  background-color: #F7F9FA;
+  box-sizing: border-box;
+  background-color: #f7f9fa;
+  position: absolute;
 `;
 
 export const AsidePanel = styled.div`
   width: 80%;
+  padding: 20px;
   margin: 0 auto;
 `;
 
 export const AsideFooter = styled.footer`
-  
+  width: 80%;
+  padding: 20px;
+  margin: 0 auto;
 `;
 
 export const Logo = styled.a`
   width: 30px;
   height: 30px;
-  background: url('../../imagens/Dropbox_Icon.svg');
+  background: url("../../imagens/Dropbox_Icon.svg");
 `;
