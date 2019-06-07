41f0647e585ec41c0309f742508350b7d013533d
everton paixao <everton.paixao16@gmail.com>
2019-06-07 13:10:39 -0300

    components shared

---
+++ src/components/shared/index.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+ 
+export const Container = styled.div`
+  margin: 0 auto;
+  width: 100%;
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  width: 100%; 
+  min-height: 100%; 
+`;
