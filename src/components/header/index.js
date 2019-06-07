5010a3f00a7d1e34b7e7c1ab86c09afb63afcad9
everton paixao <everton.paixao16@gmail.com>
2019-06-07 17:27:21 -0300

    archive on component header  finally

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,7 +1,35 @@
 import React from "react";
 
-import { Head } from "./style";
+import {
+  Head,
+  Title,
+  Navegation,
+  Form,
+  IconSearch,
+  Input,
+  IconNotification,
+  IconSingIn
+} from "./style";
+import { Container } from "../shared";
+
+import icon from "../../imagens/search.svg";
+import singin from "../../imagens/folder (1).svg";
+import notif from "../../imagens/notification (1).svg";
 
 export const Header = () => {
-  return <Head />;
+  return (
+    <Head>
+      <Container>
+        <Title>dropbox</Title>
+        <Navegation>
+          <Form>
+            <IconSearch src={icon} />
+            <Input placeholder="Buscar" />
+          </Form>
+          <IconNotification src={notif} />
+          <IconSingIn src={singin} />
+        </Navegation>
+      </Container>
+    </Head>
+  );
 };
--- src/components/header/style.js
+++ src/components/header/style.js
@@ -1,7 +1,72 @@
 import styled from "styled-components";
 
 export const Head = styled.header`
-  width: 100%;
-  height: 70px;
-  background-color: #45ff;
+  width: calc(100% - 240px);
+  height: 100px;
+  position: absolute;
+  top: 0px;
+  left: 240px;
+
+  div {
+    width: calc(100% - 240px);
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    box-sizing: border-box;
+  }
 `;
+
+export const Title = styled.h1`
+  font-size: 20px;
+  line-height: 32px;
+  font-weight: 400;
+  color: #1b2733;
+  text-transform: capitalize;
+`;
+
+export const Navegation = styled.nav`
+  margin-right: 0;
+  width: 35%;
+  min-width: 300px;
+  left: inherit;
+  margin-left: 40px;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const Form = styled.form`
+  width: 70%;
+  display: flex;
+  align-items: center;
+  border-radius: 25px;
+  overflow: hidden;
+  border: 1px solid #c1c7cd;
+  background: #fff;
+  padding: 3px;
+  position: relative;
+  display: -webkit-box;
+  display: -ms-flexbox;
+`;
+
+export const IconSearch = styled.img`
+  width: 15px;
+  height: 15px;
+  margin-top: 2px;
+  margin-left: 2px;
+`;
+
+export const Input = styled.input`
+  border: 0px;
+  display: flex;
+  padding: 3px;
+  outline: none;
+  font-size: 1em;
+`;
+
+export const IconNotification = styled.img`
+  width: 20px;
+  height: 20px;
+`;
+
+export const IconSingIn = styled(IconNotification)``;
