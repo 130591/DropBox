d8d14783e615d94b35cae9987b2b2148d9311281
everton paixao <everton.paixao16@gmail.com>
2019-08-10 10:05:21 -0300

    appactions comp

--- src/components/appactions/index.js
+++ src/components/appactions/index.js
@@ -6,25 +6,24 @@ import { ModalBox } from "../modal";
 import folder from "../../imagens/file (1).svg";
 
 export const AppactionsMenu = props => {
+  const { onPress } = props;
   return (
-    <>
-      <Menu>
-        <ModalBox />
-        <ListMenu>
-          <MenuItem>
-            <img src={folder} alt="" />
-            <h2>Enviar arquivos</h2>
-          </MenuItem>
-          <MenuItem>
-            <img src={folder} alt="" />
-            <h2>Criar pasta</h2>
-          </MenuItem>
-          <MenuItem>
-            <img src={folder} alt="" />
-            <h2>Enviar pasta</h2>
-          </MenuItem>
-        </ListMenu>
-      </Menu>
-    </>
+    <Menu>
+      <ModalBox onPress={onPress} />
+      <ListMenu>
+        <MenuItem>
+          <img src={folder} alt="" />
+          <h2>Enviar arquivos</h2>
+        </MenuItem>
+        <MenuItem>
+          <img src={folder} alt="" />
+          <h2>Criar pasta</h2>
+        </MenuItem>
+        <MenuItem>
+          <img src={folder} alt="" />
+          <h2>Enviar pasta</h2>
+        </MenuItem>
+      </ListMenu>
+    </Menu>
   );
 };
